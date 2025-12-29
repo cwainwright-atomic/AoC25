97d05a13f988f83ddfe890b890e8606f8a550175
Christopher Wainwright <chris.wainwright@atomicmedia.co.uk>
2025-12-29 20:21:37 +0000

    Complete D2 P1 + P2

---
+++ Sources/D2/D2.swift
@@ -0,0 +1,38 @@
+//
+//  D2.swift
+//  AOC25
+//
+//  Created by Christopher Wainwright on 29/12/2025.
+//
+
+import Foundation
+
+@main
+struct D2 {
+    static func main() throws {
+        let input = try String(contentsOf: Bundle.module.url(forResource: "ids", withExtension: "txt")!, encoding: .utf8)
+        let idPairs: [ClosedRange<Int>] = input
+            .trimmingCharacters(in: .whitespacesAndNewlines)
+            .split(separator: ",")
+            .compactMap { part in
+                let sections = part.split(separator: "-", maxSplits: 1)
+                guard sections.count == 2,
+                      let lower = Int(String(sections[0])),
+                      let upper = Int(String(sections[1])) else {
+                    return nil
+                }
+                return (lower...upper)
+            }
+        
+        
+        let invalidSum1 = idPairs
+            .flatMap { $0.filter(\.invalidId1) }
+            .reduce(0) { $0 + $1 }
+        print(invalidSum1)
+        
+        let invalidSum2 = idPairs
+            .flatMap { $0.filter(\.invalidId2) }
+            .reduce(0) { $0 + $1 }
+        print(invalidSum2)
+    }
+}
